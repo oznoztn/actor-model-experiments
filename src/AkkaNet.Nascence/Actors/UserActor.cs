61e77b8910e540927c8dc12c5a858bf6e661f4c5
Ozan Özten <goldenhawkk@gmail.com>
2021-02-06 23:59:49 +0300

    switching behaviors with Become method

--- src/AkkaNet.Nascence/Actors/UserActor.cs
+++ src/AkkaNet.Nascence/Actors/UserActor.cs
@@ -13,35 +13,45 @@ namespace AkkaNet.Nascence.Actors
         {
             Console.WriteLine("UserActor Constructor");
 
-            Receive<PlayMovieMessage>(HandlePlayMovieMessage);
-            Receive<StopMovieMessage>(HandleStopMovieMessage);
+            ColorConsole.WriteLine("Setting initial behavior to 'stopped'", ConsoleColor.DarkCyan);
+            Stopped();
         }
 
-        private void HandleStopMovieMessage(StopMovieMessage stopMovieMessage)
+        // Represents the playing behavior
+        private void Playing()
         {
-            if (_currentlyWatching == null)
-            {
-                ColorConsole.WriteLineRed("ERROR: Cannot stop if nothing is playing.");
-            }
-            else
-            {
-                string temp = _currentlyWatching;
-                _currentlyWatching = null;
-                ColorConsole.WriteLineGreen($"User has stopped watching {temp}.");
-            }
+            Receive<PlayMovieMessage>(message =>
+                ColorConsole.WriteLineRed(
+                    $"ERROR: Cannot start playing another movie ({message}) before stopping the existing one."));
+            Receive<StopMovieMessage>(message => StopPlayingMovie());
+
+            ColorConsole.WriteLine("UserActor has now become 'Playing'.", ConsoleColor.DarkCyan);
         }
 
-        private void HandlePlayMovieMessage(PlayMovieMessage playMovieMessage)
+        // Represents the stopped behavior
+        private void Stopped()
         {
-            if (_currentlyWatching != null)
-            {
-                ColorConsole.WriteLineRed($"ERROR: Cannot start playing another movie ({playMovieMessage.Title}) before stopping the existing one.");
-            }
-            else
-            {
-                _currentlyWatching = playMovieMessage.Title;
-                ColorConsole.WriteLineGreen($"User is watching {playMovieMessage.Title}.");
-            }
+            Receive<PlayMovieMessage>(message => StartPlayingMovie(message.Title));
+            Receive<StopMovieMessage>(message => ColorConsole.WriteLineRed("ERROR: Cannot stop if nothing is playing."));
+
+            ColorConsole.WriteLine("UserActor has now become 'Stopped'.", ConsoleColor.DarkCyan);
+        }
+
+        private void StartPlayingMovie(string title)
+        {
+            _currentlyWatching = title;
+            ColorConsole.WriteLineGreen($"User is watching {title}.");
+
+            Become(Playing);
+        }
+
+        private void StopPlayingMovie()
+        {
+            string temp = _currentlyWatching;
+            _currentlyWatching = null;
+            ColorConsole.WriteLineGreen($"User has stopped watching {temp}.");
+
+            Become(Stopped);
         }
 
         protected override void PreStart()
